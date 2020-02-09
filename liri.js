0acd316ec3091c6228bda168ca942a7935f6e08b
mrduncle1 <dun__can@hotmail.com>
2020-02-09 14:28:31 +0800

    spotify function working

--- liri.js
+++ liri.js
@@ -1,3 +1,44 @@
+//@ts-checkts-check
+
 require("dotenv").config();
 let keys = require("./keys.js");
-let spotify = new spotify(keys.spotify)
+let Spotify = require('node-spotify-api');
+let moment = require("moment");
+
+let spotify = new Spotify(keys.spotify);
+
+let searchType = process.argv[2];
+
+// axios({
+//     method: 'get',
+//     url: 'http://bit.ly/2mTM3nY',
+//     responseType: 'stream'
+//   })
+//     .then(function (response) {
+//       response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+//     });
+
+
+function spotifyThisSong (songTitle) {
+    spotify.search({type: 'track', query: songTitle}, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // console.log(JSON.stringify(data));
+        console.log("\n\n" + data.tracks.items[0].album.artists[0].name);
+        console.log(songTitle);
+        console.log(data.tracks.items[0].preview_url);        
+        console.log(data.tracks.items[0].album.name);
+    })
+}
+
+
+if (searchType === "Spotify") {
+    let searchSong = process.argv.splice(3).join(' ');
+    spotifyThisSong(searchSong);
+} else if (searchType === "OMDB") {
+    // OmdbThisMovie();
+} else if (searchType === "SomeOtherType") {
+    // Num2();
+}
+
